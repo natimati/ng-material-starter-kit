f7cac5f115b9c25dfba52ec0529b5a253cd78d51
natimati <natmateusiak@gmail.com>
2022-11-24 11:07:55 +0100

    feat: adding correct api, fixing warnings

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeModel } from '../models/employee.model';
-import {AbstractControl, ɵFormGroupRawValue, ɵGetProperty, ɵTypedOrUntyped} from "@angular/forms";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  create(employee: { name: any; salary: any; age: AbstractControl<ɵGetProperty<ɵTypedOrUntyped<any, ɵFormGroupRawValue<any>, any>, "age">> | null }): Observable<EmployeeModel> {
-    return this._httpClient.post<EmployeeModel>('https://fakestoreapi.com/products', employee);
+  create(employee: { name: string; salary: number; age: number }): Observable<EmployeeModel> {
+    return this._httpClient.post<EmployeeModel>('https://dummy.restapiexample.com/create', employee);
   }
 }
